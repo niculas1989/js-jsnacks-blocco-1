9ab6ce737aa2b73211495ec170091f9941c1f390
niculas1989 <nicolas.maranzano@libero.it>
2022-01-11 17:01:32 +0100

    add changes in script3

--- js/script3.js
+++ js/script3.js
@@ -2,13 +2,12 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.
 
 const displayElement = document.getElementById('display');
-const times = 10;
 let sum = 0;
 let isValid = true;
 
-for (let i = 0; i < times; i++) {
+for (let i = 0; i < 10; i++) {
     const userNumber = parseInt(prompt('Inserisci un numero'));
-    if (isNaN(isNumber)) {
+    if (isNaN(userNumber)) {
         isValid = false;
     } else {
         sum += userNumber;
